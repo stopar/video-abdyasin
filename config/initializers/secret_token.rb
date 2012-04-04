913390059317559cf4e495f56947a0d5a9a7542e
Muzafffar Ariff <mus.ariff@gmail.com>
2012-04-04 11:25:12 +0800

    Initial base setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ustaz::Application.config.secret_token = '7ee82665febacbe45f3867ca7aacd0e314d5a4cbc9c4977d3baa0c5d21f0c4cc9177322795847626490119efdafa4f2fafff92371e3bbbf498e1bf343fa4c8ea'
